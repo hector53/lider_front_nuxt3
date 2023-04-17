d2fb5c3183a4111d796f4d96e7a3839cf5625599
unknown <hectoracosta5@gmail.com>
2023-04-17 07:08:45 -0400

    domain

--- src/composables/urlApi.ts
+++ src/composables/urlApi.ts
@@ -1,2 +1,2 @@
-export const urlApi = "http://lider-back.ugpzb0gcxa-xmz4qee1x42o.p.temp-site.link";
+export const urlApi = "https://backend.lider.io";
 //export const urlApi = "http://localhost:4000";
